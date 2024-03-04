f23ce742c42763842ae17e5d81596719b4da96d1
Ramzi Fkaïer <ramzifk@hotmail.fr>
2024-03-04 05:24:11 +0100

    Created configmap

--- Partie2/bootMaster.sh
+++ Partie2/bootMaster.sh
@@ -9,6 +9,7 @@ export INSTALL_K3S_EXEC="server -i 192.168.56.110 --advertise-address=192.168.56
 sudo curl -sfL https://get.k3s.io | sh -
 echo "alias k='kubectl'" >> /home/vagrant/.bashrc
 sudo modprobe dummy && sudo ip link add eth1 type dummy && sudo ip addr add 192.168.56.110/24 dev eth1 && sudo ip link set dev eth1 up && sudo ip link set dev eth1 multicast on
-#
-
-
+kubectl create configmap app1-html --from-file=/vagrant/app/app1/app1.html
+kubectl create configmap app2-html --from-file=/vagrant/app/app2/app2.html
+kubectl create configmap app3-html --from-file=/vagrant/app/app3/app3.html
+kubectl apply -f /vagrant/deployment.yaml
